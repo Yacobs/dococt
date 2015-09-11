0bb59b22bffaadef177b6fff2a7db98eaa6d502a
AJ <aajacobs27@gmail.com>
2015-09-11 09:26:33 -0400

    Starting python support

--- controllers/docupload.js
+++ controllers/docupload.js
@@ -2,20 +2,26 @@
  * POST /
  * Upload a document.
  */
-exports.docUpload = function(req, res) {
-	/*
-	if(done==true)
-	{
-		console.log(req.files);
-		res.end("File uploaded.");
-	}
-	*/
-	
+exports.docUpload = function(req, res) 
+{
 	console.log(req.header);
 	console.log(req.file);
 	
 	if (typeof req.file == 'undefined') res.redirect('/');
-	else res.redirect('docviewer?file=' + req.file.filename);
+	else 
+	{
+		var python = require('child_process').spawn(
+		'python',
+		['pdf2txt.py', '-o '+ req.file.filename + '.txt', req.file.filename ]
+		);
+		
+		python.on('close', function(code){
+			if (code !== 0) console.log('Failed');
+			else console.log('Succeeded.');
+		});
+		
+		res.redirect('docviewer?file=' + req.file.filename);
+	}
 };
 
 exports.docViewer = function(req, res) 
